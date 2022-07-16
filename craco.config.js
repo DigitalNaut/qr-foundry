905fbfaa7f617df4f7ffeb9f385c0df5abd2e9b9
John B <johnbernalfsd@gmail.com>
2022-07-16 17:39:27 -0500

    Let there be code!

---
+++ craco.config.js
@@ -0,0 +1,34 @@
+const webpack = require("webpack");
+
+module.exports = {
+  webpack: {
+    configure: {
+      resolve: {
+        fallback: {
+          module: "empty",
+          dgram: "empty",
+          dns: "mock",
+          fs: "empty",
+          http2: "empty",
+          net: "empty",
+          tls: "empty",
+          child_process: "empty",
+          process: require.resolve("process/browser"),
+          zlib: require.resolve("browserify-zlib"),
+          stream: require.resolve("stream-browserify"),
+          util: require.resolve("util"),
+          buffer: require.resolve("buffer"),
+          asset: require.resolve("assert"),
+        },
+      },
+    },
+    plugins: {
+      add: [
+        new webpack.ProvidePlugin({
+          Buffer: ["buffer", "Buffer"],
+          process: "process/browser",
+        }),
+      ],
+    },
+  },
+};
